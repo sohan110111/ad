ebf06d88106d03cb2ece986e937ea0e1b61af42d
sohan110111 <sohan110111@gmail.com>
2020-03-13 01:48:30 +0600

    ==,=== access check in condition

---
+++ double-vs-triple.js
@@ -0,0 +1,44 @@
+const first = 2;
+const second = 2;
+if(first == second) { 
+    console.log("Condition is true.");
+}
+else {
+    consol.log("Condition is false.");
+}
+
+const third = 2;
+const forth = "2";
+if(third == forth) { 
+    console.log("Condition is true.");
+}
+else {
+    consol.log("Condition is false.");
+}
+
+const fifth = 2;
+const sixth = "2";
+if(fifth === sixth) { // here check type and value 
+    console.log("Condition is true.");
+}
+else {
+    console.log("Condition is false.");
+}
+
+const value1 = 1;
+const value2 = true;
+if (value1 == value2) { // because true == 1 false == 0
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+
+const value3 = 1;
+const value4 = true;
+if (value3 === value4) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
